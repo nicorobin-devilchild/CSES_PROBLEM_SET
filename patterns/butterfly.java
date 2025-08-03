c4c0eb42353982cabbbecaf16f55449eac72b62b
nicorobin-devilchild <asreethaguvvala@gamil.com>
2025-08-03 20:48:11 +0530

    Butterfly

---
+++ patterns/butterfly.java
@@ -0,0 +1,27 @@
+class Butterfly
+{
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            System.out.print("*");
+            for(int j=0;j<2*(n-i-1);j++)
+            System.out.print(" ");
+            for(int j=0;j<=i;j++)
+            System.out.print("*");
+            System.out.println();
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=i;j++)
+            System.out.print("*");
+            for(int j=0;j<2*(n-i-1);j++)
+            System.out.print(" ");
+            for(int j=0;j<=i;j++)
+            System.out.print("*");
+            System.out.println();
+        }
+        
+    }
+}
